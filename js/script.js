5947082d4d614a2dcb0749217021317715ccad92
Prantho Kumer <57418052+prantho-kumer@users.noreply.github.com>
2023-09-23 03:05:12 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
-const subCloseElement = document.querySelector(".sub-close");
-const parentUlElement = subCloseElement.parentElement;
 
-subCloseElement.addEventListener("click", function () {
-  parentUlElement.style.display = "none";
+
+// const subCloseElements = document.querySelectorAll(".sub-close");
+
+// subCloseElements.forEach((subCloseElement) => {
+//   subCloseElement.addEventListener("click", function () {
+//     this.parentElement.style.display = "none";
+//   });
+// });
+
+const menuLists = document.querySelectorAll("#navbarSupportedContent > ul > li > ul > li");
+menuLists.forEach((menuList) => {
+  menuList.addEventListener("click", function () {
+    const getUl = this.querySelector("ul");
+    if (getUl.style.display == "none") {
+      console.log("hidden");
+      getUl.style.display = "block";
+    } else {
+      console.log("shown");
+    getUl.style.display = "none";
+    }
+    
+      
+  });
 });
